faf786c36eca5ac01f4ff4be455041b4d585015a
Gabriel Leclerc <gleclerc.yk1@gmail.com>
2023-10-20 16:45:02 -0600

    tests

--- src/testing/sortingTest.java
+++ src/testing/sortingTest.java
@@ -4,11 +4,40 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import utility.sorting;
+
 public class sortingTest {
 
 	@Test
-	void test() {
-		fail("Not implemented yet");
+	public void testBubbleSort() {
+		double[] arr = { 3.0, 1.0, 4.0, 2.0, 5.0 };
+		double[] expected = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+		sorting.bubbleSort(arr);
+		assertArrayEquals(expected, arr, 0.0);
+	}
+
+	@Test
+	public void testQuickSort() {
+		double[] arr = { 3.0, 1.0, 4.0, 2.0, 5.0 };
+		double[] expected = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+		sorting.quickSort(arr, 0, arr.length - 1);
+		assertArrayEquals(expected, arr, 0.0);
+	}
+
+	@Test
+	public void testSelectionSort() {
+		int[] arr = { 3, 1, 4, 2, 5 };
+		int[] expected = { 1, 2, 3, 4, 5 };
+		sorting.selectionSort(arr);
+		assertArrayEquals(expected, arr);
+	}
+
+	@Test
+	public void testMergeSort() {
+		int[] arr = { 38, 27, 43, 3, 9, 82, 10 };
+		int[] expected = { 3, 9, 10, 27, 38, 43, 82 };
+		int[] sortedArray = sorting.MergeSort.sort(arr);
+		assertArrayEquals(expected, sortedArray);
 	}
 
 }
