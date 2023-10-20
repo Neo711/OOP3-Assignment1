447b40da51a87f8d4b341facdc84c68c448a793d
Benjamin Roskey <benjamin.roskey@edu.sait.ca>
2023-10-20 16:50:37 -0600

    test

--- src/utility/sorting.java
+++ src/utility/sorting.java
@@ -19,7 +19,18 @@ public class sorting {
             }
         }
     }
-
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int current = array[i];
+            int j = i - 1;
+            while(j >= 0 && array[j] > current) {
+                array[j+1] = array[j];
+                j = j -1;
+            }
+            array[j+1] = current;
+        }
+    }
+    
     public static void quickSort(double[] arr, int low, int high) {
         if (low < high) {
             int pi = partition(arr, low, high);
