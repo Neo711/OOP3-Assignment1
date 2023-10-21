42352eec5735c24a9e530f4ed2a34b369ca54083
Neo711 <111005592+Neo711@users.noreply.github.com>
2023-10-20 19:34:14 -0600

    Main, FileInput, FileOutput,

--- lib/filehandling/FileInput.java
+++ lib/filehandling/FileInput.java
@@ -1,5 +1,20 @@
 package lib;
 
-public class FileInput {
+import java.io.*;
 
+public class FileInput {
+    private BufferedReader in;
+
+    public FileInput(String fileName) throws FileNotFoundException {
+        FileReader fr = new FileReader(new File(fileName));
+        in = new BufferedReader(fr);
+    }
+
+    public String readLine() throws IOException {
+        return in.readLine();
+    }
+
+    public void close() throws IOException {
+        in.close();
+    }
 }
--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,10 +1,49 @@
 package main;
 
+import java.io.IOException;
+
 public class Driver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        String filePath = null;
 
-	}
+        // Parse command line arguments
+        for (int i = 0; i < args.length - 1; i++) {
+            if ("-f".equals(args[i])) {
+                filePath = args[i + 1];
+                break;
+            }
+        }
 
+        if (filePath == null) {
+            System.out.println("Please specify a file using the -f option.");
+            return;
+        }
+
+        // Assuming a maximum of 100 shapes for simplicity
+        String[] shapes = new String[100];
+        int shapeCount = 0;
+
+        // Use FileInput class to read from file
+        try (FileInput fileInput = new FileInput(filePath)) {
+            String shape;
+            while ((shape = fileInput.readLine()) != null && shapeCount < 100) {
+                shapes[shapeCount++] = shape;
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file: " + e.getMessage());
+        }
+
+        // Use FileOutput class to write to an output file
+        try (FileOutput fileOutput = new FileOutput("output.txt")) {
+            for (int i = 0; i < shapeCount; i++) {
+                fileOutput.writeLine(shapes[i]);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file: " + e.getMessage());
+        }
+
+        System.out.println("Shapes processed and written to output.txt!");
+    }
 }
+
