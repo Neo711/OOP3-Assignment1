efab15ef56de2cff374228a74fa55e6c99e12880
Neo711 <cyrildizon04@gmail.com>
2023-10-25 18:42:25 -0600

    Removed ArrayList

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,30 +1,30 @@
+package main;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-
 import shapes.Shape;
-
 
 public class Driver {
 
     public static void main(String[] args) {
         try {
-            List<Shape> shapes = readShapesFromFile("path_to_shapes.txt");
+            Shape[] shapes = readShapesFromFile("path_to_shapes.txt");
             for (Shape shape : shapes) {
-                System.out.println(shape);
+                if (shape != null) {
+                    System.out.println(shape);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static List<Shape> readShapesFromFile(String filePath) throws Exception {
-        List<Shape> shapes = new ArrayList<>();
+    public static Shape[] readShapesFromFile(String filePath) throws Exception {
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             int numShapes = Integer.parseInt(br.readLine().trim());
+            Shape[] shapes = new Shape[numShapes];
             for (int i = 0; i < numShapes; i++) {
                 String shapeType = br.readLine().trim();
                 Class<?> shapeClass = Class.forName(shapeType);
@@ -40,15 +40,16 @@ public class Driver {
                 initializeMethod.invoke(shape, param1, param2);
 
                 if (shape instanceof Shape) {
-                    shapes.add((Shape) shape);
+                    shapes[i] = (Shape) shape;
                 } else {
                     System.out.println("The class " + shapeType + " is not an instance of Shape. Skipping...");
                 }
             }
+            return shapes;
         }
-        return shapes;
     }
 }
+
 
 
 
