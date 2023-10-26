fd1254c54ec424e828d99ef136f5e7ab36cb8519
Gabriel Leclerc <gleclerc.yk1@gmail.com>
2023-10-25 20:25:17 -0600

    jar file changes

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,51 +1,103 @@
+package main;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.lang.reflect.Constructor;
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Method;
+import shapes.Shape;
 
+public class Driver {
 
-public class ShapeFileReader {
+    public static void main(String[] args) {
+        String filePath = args[0];
+        String compareType = args[1];
+        String sortType = args[2];
 
-    public static void main(String[] args) throws Exception {
-        List<Shape> shapes = readShapesFromFile("path_to_shapes.txt");
-        for (Shape shape : shapes) {
-            System.out.println(shape);
+        setShapeCompareType(compareType);
+        String sortTypeName = setSortMethod(sortType);
+
+        try {
+            Shape[] shapes = readShapesFromFile(filePath);
+            Method sortMethod = Shape.class.getMethod(sortTypeName, Shape[].class);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public static List<Shape> readShapesFromFile(String filePath) throws Exception {
-        // List<Shape> shapes = new ArrayList<>();//get rid
+    public static Shape[] readShapesFromFile(String filePath) throws Exception {
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             int numShapes = Integer.parseInt(br.readLine().trim());
+            Shape[] shapes = new Shape[numShapes];
             for (int i = 0; i < numShapes; i++) {
                 String shapeType = br.readLine().trim();
                 Class<?> shapeClass = Class.forName(shapeType);
+
+                double param1 = Double.parseDouble(br.readLine().trim());
+                double param2 = Double.parseDouble(br.readLine().trim());
 
                 Constructor<?> constructor = shapeClass.getDeclaredConstructor();
                 constructor.setAccessible(true);
                 Object shape = constructor.newInstance();
 
-                Method method = shapeClass.getMethod(shapeType);
-                
-                // //if (shapeClass.equals(Circle.class)) {
-                //     double radius = Double.parseDouble(br.readLine().trim());
-                //     Constructor<?> constructor = shapeClass.getConstructor(double.class);
-                //     Shape circle = (Shape) constructor.newInstance(radius);
-                //     shapes.add(circle);
-                // } else if (shapeClass.equals(Rectangle.class)) {
-                //     String[] dimensions = br.readLine().split(" ");
-                //     double width = Double.parseDouble(dimensions[0]);
-                //     double height = Double.parseDouble(dimensions[1]);
-                //     Constructor<?> constructor = shapeClass.getConstructor(double.class, double.class);
-                //     Shape rectangle = (Shape) constructor.newInstance(width, height);
-                //     shapes.add(rectangle);
-                // } else {
-                //     throw new IllegalArgumentException("Unknown shape: " + shapeType);
-                // }
+                Method initializeMethod = shapeClass.getMethod("initialize", double.class, double.class);
+                initializeMethod.invoke(shape, param1, param2);
+
+                if (shape instanceof Shape) {
+                    shapes[i] = (Shape) shape;
+                } else {
+                    System.out.println("The class " + shapeType + " is not an instance of Shape. Skipping...");
+                }
             }
+            return shapes;
         }
-        return shapes;
+    }
+
+    public static void setShapeCompareType(String compareType) {
+        if (compareType == "h") {
+            Shape.compareType = "height";
+        } else if (compareType == "v") {
+            Shape.compareType = "volume";
+        } else if (compareType == "a") {
+            Shape.compareType = "base area";
+        } else {
+            System.out.println("Invalid compare type. Please enter 'h' or 'v'.");
+        }
+        ;
+    }
+
+    public static String setSortMethod(String sortType) throws NoSuchMethodException, SecurityException {
+        String sortMethod = "";
+        if (sortType == "b") {
+            return "bubbleSort";
+        } else if (sortType == "s") {
+            return "selectionSort";
+        } else if (sortType == "i") {
+            return "insertionSort";
+        } else if (sortType == "m") {
+            return "mergeSort";
+        } else if (sortType == "q") {
+            return "quickSort";
+        } else if (sortType == "z") {
+            return "radixSort";
+        } else {
+            System.out.println("Invalid sort type. Please enter 'b', 's', 'i', or 'm'.");
+        }
     }
 }
 
+// //if (shapeClass.equals(Circle.class)) {
+// double radius = Double.parseDouble(br.readLine().trim());
+// Constructor<?> constructor = shapeClass.getConstructor(double.class);
+// Shape circle = (Shape) constructor.newInstance(radius);
+// shapes.add(circle);
+// } else if (shapeClass.equals(Rectangle.class)) {
+// String[] dimensions = br.readLine().split(" ");
+// double width = Double.parseDouble(dimensions[0]);
+// double height = Double.parseDouble(dimensions[1]);
+// Constructor<?> constructor = shapeClass.getConstructor(double.class,
+// double.class);
+// Shape rectangle = (Shape) constructor.newInstance(width, height);
+// shapes.add(rectangle);
+// } else {
+// throw new IllegalArgumentException("Unknown shape: " + shapeType);
+// }
