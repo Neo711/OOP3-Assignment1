b9c6ff9481d614ce1fcecef75fd9afb6b4f2fe3b
benroskey <120222153+benroskey@users.noreply.github.com>
2023-10-25 19:07:11 -0600

    Update SortingTest.java
    added height, base area, and volume testing for both insertion and radix sort

--- src/testing/SortingTest.java
+++ src/testing/SortingTest.java
@@ -333,4 +333,164 @@ public class SortingTest {
 		assertArrayEquals(expected, arr);
 	}
 
+	@Test
+	public void testInsertionSortDoubles() {
+		double[] arr = { 3.0, 1.0, 4.0, 2.0, 5.0 };
+		double[] expected = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+		Sorting.insertionSort(arr);
+		assertArrayEquals(expected, arr, 0.0);
+	}
+
+	@Test
+	public void testInsertionSortShapesHeight() {
+		Shape[] arr = new Shape[5];
+		Shape[] expected = new Shape[5];
+		Shape s1 = new TriangularPrism(3.0, 3.0);
+		Shape s2 = new Cone(1.0, 1.0);
+		Shape s3 = new Cylinder(4.0, 4.0);
+		Shape s4 = new TriangularPrism(2.0, 2.0);
+		Shape s5 = new OctagonalPrism(5.0, 5.0);
+		arr[0] = s1;
+		arr[1] = s2;
+		arr[2] = s3;
+		arr[3] = s4;
+		arr[4] = s5;
+		expected[0] = s2;
+		expected[1] = s4;
+		expected[2] = s1;
+		expected[3] = s3;
+		expected[4] = s5;
+		Shape.compareType = "height";
+		Sorting.insertionSort(arr);
+		assertArrayEquals(expected, arr);
+	}
+
+	@Test
+	public void testInsertionSortShapesBaseArea() {
+		Shape[] arr = new Shape[5];
+		Shape[] expected = new Shape[5];
+		Shape s1 = new TriangularPrism(3.0, 3.0);
+		Shape s2 = new Cone(1.0, 1.0);
+		Shape s3 = new Cylinder(4.0, 4.0);
+		Shape s4 = new TriangularPrism(2.0, 2.0);
+		Shape s5 = new OctagonalPrism(5.0, 5.0);
+		arr[0] = s1;
+		arr[1] = s2;
+		arr[2] = s3;
+		arr[3] = s4;
+		arr[4] = s5;
+		expected[0] = s2;
+		expected[1] = s4;
+		expected[2] = s1;
+		expected[3] = s3;
+		expected[4] = s5;
+		Shape.compareType = "base area";
+		Sorting.insertionSort(arr);
+		assertArrayEquals(expected, arr);
+	}
+
+	@Test
+	public void testInsertionSortShapesVolume() {
+		Shape[] arr = new Shape[5];
+		Shape[] expected = new Shape[5];
+		Shape s1 = new TriangularPrism(3.0, 3.0);
+		Shape s2 = new Cone(1.0, 1.0);
+		Shape s3 = new Cylinder(4.0, 4.0);
+		Shape s4 = new TriangularPrism(2.0, 2.0);
+		Shape s5 = new OctagonalPrism(5.0, 5.0);
+		arr[0] = s1;
+		arr[1] = s2;
+		arr[2] = s3;
+		arr[3] = s4;
+		arr[4] = s5;
+		expected[0] = s2;
+		expected[1] = s4;
+		expected[2] = s1;
+		expected[3] = s3;
+		expected[4] = s5;
+		Shape.compareType = "volume";
+		Sorting.insertionSort(arr);
+		assertArrayEquals(expected, arr);
+	}
+
+	@Test
+	public void testRadixSortDoubles() {
+		double[] arr = { 3.0, 1.0, 4.0, 2.0, 5.0 };
+		double[] expected = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+		Sorting.radixSort(arr);
+		assertArrayEquals(expected, arr, 0.0);
+	}
+
+	@Test
+	public void testRadixSortShapesHeight() {
+		Shape[] arr = new Shape[5];
+		Shape[] expected = new Shape[5];
+		Shape s1 = new TriangularPrism(3.0, 3.0);
+		Shape s2 = new Cone(1.0, 1.0);
+		Shape s3 = new Cylinder(4.0, 4.0);
+		Shape s4 = new TriangularPrism(2.0, 2.0);
+		Shape s5 = new OctagonalPrism(5.0, 5.0);
+		arr[0] = s1;
+		arr[1] = s2;
+		arr[2] = s3;
+		arr[3] = s4;
+		arr[4] = s5;
+		expected[0] = s2;
+		expected[1] = s4;
+		expected[2] = s1;
+		expected[3] = s3;
+		expected[4] = s5;
+		Shape.compareType = "height";
+		Sorting.radixSort(arr);
+		assertArrayEquals(expected, arr);
+	}
+
+	@Test
+	public void testRadixSortShapesBaseArea() {
+		Shape[] arr = new Shape[5];
+		Shape[] expected = new Shape[5];
+		Shape s1 = new TriangularPrism(3.0, 3.0);
+		Shape s2 = new Cone(1.0, 1.0);
+		Shape s3 = new Cylinder(4.0, 4.0);
+		Shape s4 = new TriangularPrism(2.0, 2.0);
+		Shape s5 = new OctagonalPrism(5.0, 5.0);
+		arr[0] = s1;
+		arr[1] = s2;
+		arr[2] = s3;
+		arr[3] = s4;
+		arr[4] = s5;
+		expected[0] = s2;
+		expected[1] = s4;
+		expected[2] = s1;
+		expected[3] = s3;
+		expected[4] = s5;
+		Shape.compareType = "base area";
+		Sorting.radixSort(arr);
+		assertArrayEquals(expected, arr);
+	}
+
+	@Test
+	public void testRadixSortShapesVolume() {
+		Shape[] arr = new Shape[5];
+		Shape[] expected = new Shape[5];
+		Shape s1 = new TriangularPrism(3.0, 3.0);
+		Shape s2 = new Cone(1.0, 1.0);
+		Shape s3 = new Cylinder(4.0, 4.0);
+		Shape s4 = new TriangularPrism(2.0, 2.0);
+		Shape s5 = new OctagonalPrism(5.0, 5.0);
+		arr[0] = s1;
+		arr[1] = s2;
+		arr[2] = s3;
+		arr[3] = s4;
+		arr[4] = s5;
+		expected[0] = s2;
+		expected[1] = s4;
+		expected[2] = s1;
+		expected[3] = s3;
+		expected[4] = s5;
+		Shape.compareType = "volume";
+		Sorting.radixSort(arr);
+		assertArrayEquals(expected, arr);
+	}
+
 }
